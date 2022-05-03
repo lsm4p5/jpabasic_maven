20eb7b1acf76632934ca3c1726bcd16d94efa876
lsm4p5 <lsm4p5@gmail.com>
2022-05-03 17:38:32 +0900

    테스트도 올린다.

---
+++ src/test/java/jpabasic/MainTest.java
@@ -0,0 +1,14 @@
+package jpabasic;
+
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    public void test1(){
+
+        System.out.println("MainTest.test1+ 시작");
+    }
+
+}
